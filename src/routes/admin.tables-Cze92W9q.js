5782db1ef78d9bd959e55e7bf049f514292d2293
Lelel65693 <145902674+Lelel65693@users.noreply.github.com>
2026-06-05 18:03:48 +0400

    Delete src/routes/admin.tables-Cze92W9q.js

--- src/routes/admin.tables-Cze92W9q.js
+++
@@ -1,9 +0,0 @@
-import { jsx } from "react/jsx-runtime";
-import "./client-Cg-358lU.js";
-import "@supabase/supabase-js";
-const SplitErrorComponent = ({
-  error
-}) => /* @__PURE__ */ jsx("div", { className: "p-8 text-destructive bg-destructive/10 border border-destructive/20 rounded-lg m-4", children: error.message });
-export {
-  SplitErrorComponent as errorComponent
-};
